feb4b2cc3e8e1a7321a2a784baaf845de8f08df3
soni-aks <akshatsoni1996@gmail.com>
2022-01-25 01:29:53 +0530

    subtraction added

--- JavaPractice/src/Sample.java
+++ JavaPractice/src/Sample.java
@@ -4,8 +4,13 @@ public class Sample {
 	{
 		return x+y;
 	}
+	static int sub(int x,int y)
+	{
+		return x-y;
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("sum="+add(2,4));
+		System.out.println("sub="+sub(4,2));
 	}
 }
